8452da664b20cc64fa50f39cde19e07f71f80f88
mirthfulLee <lihuan0530@gmail.com>
2025-01-04 20:56:24 +0800

    init

---
+++ autokani/src/lib.rs
@@ -0,0 +1,204 @@
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, FnArg, Item, ItemFn, Pat, Type, TypeReference};
+
+#[proc_macro_attribute]
+pub fn kani_test(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as Item);
+    let func = match input {
+        Item::Fn(func) => func,
+        _ => {
+            return quote! {
+                compile_error!("`kani_test` can only be used on functions.");
+            }
+            .into();
+        }
+    };
+
+    let func_name = &func.sig.ident;
+    let inputs = &func.sig.inputs;
+    // let output = &func.sig.output;
+
+    let harness_name = quote::format_ident!("check_{}", func_name);
+
+    let mut harness_body = Vec::new();
+    let mut call_args: Vec<proc_macro2::TokenStream> = Vec::new();
+
+    for arg in inputs {
+        if let FnArg::Typed(pat_type) = arg {
+            let arg_name = match &*pat_type.pat {
+                syn::Pat::Ident(pat_ident) => pat_ident.ident.to_string(),
+                _ => "arg".to_string(),
+            };
+            let arg_type = &pat_type.ty;
+            let is_mut = matches!(&*pat_type.pat, syn::Pat::Ident(pat_ident) if pat_ident.mutability.is_some());
+            let init_stmt = init_for_type(&arg_name, &arg_type, is_mut);
+            harness_body.push(init_stmt);
+
+            let arg_ident = quote::format_ident!("{}", arg_name);
+            call_args.push(quote! { #arg_ident });
+        }
+    }
+
+    let call_stmt = quote! {
+        let _ = #func_name(#(#call_args),*);
+    };
+    // let call_stmt = if !call_args.is_empty() {
+    //     quote! {
+    //         let _ = #func_name(#(#call_args),*);
+    //     }
+    // } else {
+    //     quote! {
+    //         let _ = #func_name();
+    //     }
+    // };
+    harness_body.push(call_stmt);
+
+    let output = quote! {
+        #func
+
+        #[cfg(kani)]
+        #[kani::proof]
+        #[kani::unwind(64)]
+        pub fn #harness_name() {
+            #(#harness_body)*
+        }
+    };
+
+    output.into()
+}
+
+fn init_for_type(arg_name: &str, arg_type: &Type, is_mut: bool) -> proc_macro2::TokenStream {
+    const ARR_LIMIT: usize = 16;
+    // let mutability = quote::format_ident!("{}", if is_mut { "mut" } else { "" });
+    let mutability = if is_mut { quote!(mut) } else { quote!() };
+    let arg_ident = quote::format_ident!("{}", arg_name);
+    let init_stmt = match arg_type {
+        // Limit the range of u32 and u64 to avoid overflow errors
+        Type::Path(ref type_path)
+            if type_path.path.is_ident("u32") || type_path.path.is_ident("u64") =>
+        {
+            quote! {
+                let #mutability #arg_ident = kani::any();
+                kani::assume(#arg_ident < 100000000);
+            }
+        }
+        Type::Path(ref type_path)
+            if type_path.path.is_ident("i32") || type_path.path.is_ident("i64") =>
+        {
+            quote! {
+                let #mutability #arg_ident = kani::any();
+                kani::assume(#arg_ident < 100000000 && #arg_ident > -100000000);
+            }
+        }
+        Type::Path(ref type_path) if matches!(type_path.path.segments.last(), Some(_)) => {
+            let final_seg = type_path.path.segments.last().unwrap();
+            let inner_type = &final_seg.arguments;
+            // TODO: support more types, e.g., HashMap, HashSet, etc.
+            if final_seg.ident == "Vec" {
+                let vec_type = inner_type.clone();
+                if let syn::PathArguments::AngleBracketed(args) = vec_type {
+                    if let Some(syn::GenericArgument::Type(ty)) = args.args.first() {
+                        quote! {
+                            let #mutability #arg_ident = kani::vec::any_vec::<#ty, #ARR_LIMIT>();
+                        }
+                    } else {
+                        quote! {
+                            compile_error!("Unsupported Vec Type");
+                        }
+                    }
+                } else {
+                    quote! {
+                        compile_error!("Unsupported Vec Pattern");
+                    }
+                }
+            } else if final_seg.ident == "Option" {
+                quote! {
+                let #mutability #arg_ident = if kani::any() { Some(kani::any()) } else { None };
+                }
+            } else if final_seg.ident == "Result" {
+                quote! {
+                let #mutability #arg_ident: Result<_, ()> = if kani::any() { Ok(kani::any()) } else { Err(()) };
+                }
+            } else {
+                let final_ident = &final_seg.ident;
+                quote! {
+                    let #mutability #arg_ident: #final_ident = kani::any();
+                }
+            }
+        }
+        Type::Path(ref type_path)
+            if type_path.path.is_ident("String") || type_path.path.is_ident("str") =>
+        {
+            init_for_string(arg_name, is_mut)
+        }
+        // Type::Path(_) => {
+        //     quote! {
+        //         let #mutability #arg_ident = kani::any();
+        //     }
+        // }
+        Type::Array(type_arr) => {
+            let arr_type = &type_arr.elem;
+            let arr_len = &type_arr.len;
+            quote! {
+                let #mutability #arg_ident = kani::any::<[#arr_type; #arr_len]>();
+            }
+        }
+        Type::Slice(type_slice) => {
+            let slice_type = &type_slice.elem;
+            quote! {
+                let #mutability #arg_ident = kani::vec::any_vec::<#slice_type, #ARR_LIMIT>();
+            }
+        }
+        Type::Tuple(tuple) => {
+            let tuple_elems = tuple.elems.iter().map(|elem| {
+                let elem_name = quote::format_ident!("{}_elem", arg_ident);
+                let elem_init = init_for_type(&elem_name.to_string(), elem, is_mut);
+                quote! {
+                    {
+                        #elem_init
+                        #elem_name
+                    }
+                }
+            });
+            quote! {
+                let #mutability #arg_ident = (#(#tuple_elems),*);
+            }
+        }
+        Type::Reference(TypeReference {
+            elem, mutability, ..
+        }) => {
+            let obj_name = quote::format_ident!("{}_obj", arg_ident);
+            let obj_init =
+                init_for_type(&obj_name.to_string(), elem, matches!(mutability, Some(_)));
+            quote! {
+                #obj_init
+                let #mutability #arg_ident = &#mutability #obj_name;
+            }
+        }
+        // Type::Ptr(type_ptr) => {
+        //     let ptr_type = &type_ptr.elem;
+        //     let obj_name = quote::format_ident!("{}_obj", arg_ident);
+        //     let obj_init = init_for_type(&obj_name.to_string(), ptr_type, is_mut);
+        //     quote! {
+        //         #obj_init
+        //         let #mutability #arg_ident = &mut #obj_name as *mut _;
+        //     }
+        // }
+        _ => quote! {
+            compile_error!("Unsupported argument type for `kani_test` macro.");
+        },
+    };
+    init_stmt
+}
+
+fn init_for_string(arg_name: &str, is_mut: bool) -> proc_macro2::TokenStream {
+    const STRING_LIMIT: usize = 8;
+    let mutability = if is_mut { quote!(mut) } else { quote!() };
+    let arg_ident = quote::format_ident!("{}", arg_name);
+    let arr_name = quote::format_ident!("{}_arr", arg_ident);
+    quote! {
+        let #arr_name = kani::any::<[char; #STRING_LIMIT]>();
+        let #mutability #arg_ident = String::from_iter(#arr_name);
+    }
+}
---
+++ simple_kani/src/backup.rs
@@ -0,0 +1,189 @@
+//// Basic Types //////////////////////////////////////////////
+#[kani_test]
+pub fn u8_input(s: u8) {
+    let _ = s;
+}
+#[kani_test]
+pub fn i16_input(s: i16) {
+    let _ = s;
+}
+#[kani_test]
+pub fn f16_input(s: f32) {
+    let _ = s;
+}
+#[kani_test]
+pub fn bool_input(s: bool) {
+    let _ = s;
+}
+
+//// String //////////////////////////////////////////////
+#[kani_test]
+pub fn string_input(s: String) {
+    let _ = s;
+}
+
+//// str //////////////////////////////////////////////
+#[kani_test]
+pub fn str_input(s: &str) {
+    let _ = s;
+}
+
+//// multiple params //////////////////////////////////////////////
+#[kani_test]
+pub fn multi_param(a: i16, b: u8, c: f32, d: bool) {
+    let _ = a;
+    let x = b as f32 + c ;
+    if d {
+        let y = b + c as u8;
+    }
+}
+
+//// slice //////////////////////////////////////////////
+#[kani_test]
+pub fn char_to_str(stream: &[u8]) -> String {
+    unsafe { String::from_utf8_unchecked(stream.to_vec()) }
+}
+
+#[kani_test]
+pub fn slice_input(stream: [u8; 10]) -> String {
+    unsafe { String::from_utf8_unchecked(stream.to_vec()) }
+}
+
+#[kani_test]
+pub fn slice_input2(stream: &[i32; 10]) {
+    let _ = stream;
+}
+
+
+//// Tuple //////////////////////////////////////////////
+
+#[kani_test]
+pub fn multi_param(a: (i16, u8), c: f32, d: bool) {
+    let _ = a;
+    let x = a.0 as f32 + c ;
+    if d {
+        let y = a.1 + c as u8;
+    }
+}
+
+#[kani_test]
+pub fn multi_param1(a: (i16, u8), c: f32, v: &[u8]) {
+    let _ = a;
+    let x = a.0 as f32 + c ;
+    let y = a.1 + c as u8;
+    let _ = v[y as usize];
+}
+
+
+//// Vec //////////////////////////////////////////////
+
+#[kani_test]
+pub fn to_str(stream: Vec<u8>) -> String {
+    unsafe { String::from_utf8_unchecked(stream) }
+}
+
+#[kani_test]
+pub fn multi_param1(a: (i16, u8), b: f32, v: Vec<i32>) {
+    let y = a.1 + b as u8;
+    let _ = v[y as usize];
+}
+
+//// Reference //////////////////////////////////////////////
+#[kani_test]
+pub fn to_str(stream: &Vec<i16>) {
+    let _ = stream.clone();
+}
+
+#[kani_test]
+pub fn multi_param1(a: (i16, u8), b: &f32, v: Vec<i32>) {
+    let y = a.1 + *b as u8;
+    let _ = v[y as usize];
+}
+
+#[kani_test]
+pub fn char_to_str2(stream: (u32, char), a: &f32) {
+    let _ = a;
+}
+
+//// Mutability //////////////////////////////////////////////
+
+#[kani_test]
+pub fn u8_input(s:&mut u8) {
+    let _ = s;
+}
+
+#[kani_test]
+pub fn multi_param_mut(a: (i16, u8), b: &mut f32, v: Vec<i32>) {
+    *b += 1.0;
+    let y = a.1 + *b as u8;
+    let _ = v[y as usize];
+}
+
+#[kani_test]
+pub fn multi_param_mut1(a: (i16, u8), b: &mut f32, mut v: Vec<i32>) {
+    *b += 1.0;
+    let y = a.1 + *b as u8;
+    v[y as usize] = 0;
+}
+
+#[kani_test]
+pub fn initialize_prefix(length: usize, buffer: &mut [u8]) {
+    // Let's just ignore invalid calls
+    if length > buffer.len() {
+        return;
+    }
+
+    for i in 0..=length {
+        buffer[i] = 0;
+    }
+}
+
+//// Raw Pointer //////////////////////////////////////////////
+
+
+
+//// Structs //////////////////////////////////////////////
+
+pub struct Array {
+    data: Vec<i32>,
+    len: usize,
+    capacity: usize,
+}
+
+impl Array {
+    pub fn new(cap: usize) -> Self {
+        Array {
+            data: Vec::with_capacity(cap),
+            len: 0,
+            capacity: cap,
+        }
+    }
+    pub fn push(&mut self, val: i32) {
+        if self.len == self.capacity {
+            return;
+        }
+        self.data.push(val);
+        self.len += 1;
+    }
+    pub fn pop(&mut self) -> Option<i32> {
+        if self.len == 0 {
+            return None;
+        }
+        self.len -= 1;
+        self.data.pop()
+    }
+    /// # Safety
+    /// This function is unsafe because it doesn't check if the index is out of bounds.
+    pub unsafe fn get_unchecked(&self, index: usize) -> i32 {
+        *self.data.get_unchecked(index)
+    }
+}
+pub fn get_unsound(arr: &Array, index: usize) -> Option<i32> {
+    Some(unsafe { arr.get_unchecked(index) })
+}
+pub fn get_sound(arr: &Array, index: usize) -> Option<i32> {
+    if index >= arr.len {
+        return None;
+    }
+    Some(unsafe { arr.get_unchecked(index) })
+}
